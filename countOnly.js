cf3fc2f7e40e061791a098e25d8aff4cdb516474
elyeel <elyeel@yahoo.com>
2019-07-24 23:16:27 +0000

    select only certain ppl

--- countOnly.js
+++ countOnly.js
@@ -10,14 +10,16 @@ const assertEqual = function(actual, expected) {
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
-
   for (const item of allItems) {
-    if (results[item]) {
-      results[item] += 1;
-    } else {
-      results[item] = 1;
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
     }
   }
+
   return results;
 };
 
@@ -33,8 +35,17 @@ const firstNames = [
   "Joe"
 ];
 
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+//const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+const result1 = countOnly(firstNames, { "Fang": true });
+
 
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);
+/*
+if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
+    */
