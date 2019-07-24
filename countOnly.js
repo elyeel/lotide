15144de35d33882fe36402657b4f54de3773794a
elyeel <elyeel@yahoo.com>
2019-07-24 21:48:59 +0000

    completed, unlinted

--- countOnly.js
+++ countOnly.js
@@ -10,6 +10,14 @@ const assertEqual = function(actual, expected) {
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
+
+  for (const item of allItems) {
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
+  }
   return results;
 };
 
