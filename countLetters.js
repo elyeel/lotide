dc1b2679ce9605ec52918ebd268ae6368a3a3612
elyeel <elyeel@yahoo.com>
2019-07-24 23:52:56 +0000

    Added countLetters function

--- countLetters.js
+++ countLetters.js
@@ -5,3 +5,19 @@ const assertEqual = function(actual, expected) {
     console.log('❌❌❌ Assertion Failed: ' + actual + ' !== ' + expected);
   }
 };
+
+let countLetters = function(strArray){
+  const results = {};
+  for (let chr of strArray) {
+    console.log(chr);
+    if (results[chr] !== ' ') {
+      if (results[chr]) {
+        results[chr] += 1;
+      } else {
+        results[chr] = 1;
+      }
+    }
+    
+  } return results;
+};
+console.log(countLetters('lighthouse in the house'));
