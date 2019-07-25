a7600d136f92ae93a20c396d67c6cc42e577ab25
elyeel <elyeel@yahoo.com>
2019-07-25 01:01:05 +0000

    Completed w/rushing it

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('✔️ ✔️ ✔️  Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log('❌❌❌ Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+const eqArrays = function(arrOne, arrTwo) {
+  resultOne = '', resultTwo = '';
+  if (arrOne.length === arrTwo.length) {
+    for (let i = 0; i < arrOne.length; i++){
+      resultOne += arrOne[i];
+      resultTwo += arrTwo[i];
+    }
+  } else {
+    return false;
+  }
+  if (resultOne === resultTwo){
+    return true;
+  } else {
+    return false;
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
