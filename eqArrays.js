629325692097ea7f6a4fabca510b8af49a44e167
elyeel <elyeel@yahoo.com>
2019-07-25 01:11:31 +0000

    linted out

--- eqArrays.js
+++ eqArrays.js
@@ -7,16 +7,16 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arrOne, arrTwo) {
-  resultOne = '', resultTwo = '';
+  let resultOne = '', resultTwo = '';
   if (arrOne.length === arrTwo.length) {
-    for (let i = 0; i < arrOne.length; i++){
+    for (let i = 0; i < arrOne.length; i++) {
       resultOne += arrOne[i];
       resultTwo += arrTwo[i];
     }
   } else {
     return false;
   }
-  if (resultOne === resultTwo){
+  if (resultOne === resultTwo) {
     return true;
   } else {
     return false;
