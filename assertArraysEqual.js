a79844f24ad18862253bb1f1822b035c16b63f8b
elyeel <elyeel@yahoo.com>
2019-07-25 01:33:02 +0000

    Completed, maybe need a fix

---
+++ assertArraysEqual.js
@@ -0,0 +1,18 @@
+const assertArraysEqual = function(arrOne, arrTwo) {
+  let resultOne = '', resultTwo = '';
+  if (arrOne.length === arrTwo.length) {
+    for (let i = 0; i < arrOne.length; i++) {
+      resultOne += arrOne[i];
+      resultTwo += arrTwo[i];
+    }
+  } else {
+    return (console.log('❌❌❌ ' + arrOne + ' !== ' + arrTwo));
+    
+  }
+  if (resultOne === resultTwo) {
+    console.log('✔️ ✔️ ✔️  ' + arrOne + ' === ' + arrTwo);
+  } else {
+    console.log('❌❌❌ ' + arrOne + ' !== ' + arrTwo);
+  }
+};
+assertArraysEqual([1, 2, 3], [1, 3]);
