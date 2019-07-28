abe7c1f88477cb1af73dcbdac0c304e809b881eb
Risyardi Iswanto <elyeel@yahoo.com>
2019-07-28 05:14:52 +0000

    middle function done

---
+++ middle.js
@@ -0,0 +1,33 @@
+const assertArraysEqual = function(arrOne, arrTwo) {
+  let resultOne = '', resultTwo = '';
+  if (arrOne.length === arrTwo.length) {
+    for (let i = 0; i < arrOne.length; i++) {
+      resultOne += arrOne[i];
+      resultTwo += arrTwo[i];
+    }
+  } else {
+    return (console.log('❌❌❌ ' + arrOne + ' !== ' + arrTwo));
+    
+  }
+  if (resultOne === resultTwo) {
+    console.log('✔️ ✔️ ✔️  ' + arrOne + ' === ' + arrTwo);
+  } else {
+    console.log('❌❌❌ ' + arrOne + ' !== ' + arrTwo);
+  }
+};
+
+const middle = function(arr) {
+  let arrResult = [];
+  if (arr.length > 2) {//only if array length > 2
+    if (arr.length % 2 === 0) {//for even array length
+      arrResult.push(arr[((arr.length) / 2) - 1]);
+      arrResult.push(arr[(arr.length) / 2]);
+    } else {//for odd array length
+      arrResult.push(arr[Math.floor(0.5 * arr.length)]);
+    }
+  } return arrResult;
+};
+
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]);
+assertArraysEqual(middle([1, 2]),[1, 2]);
